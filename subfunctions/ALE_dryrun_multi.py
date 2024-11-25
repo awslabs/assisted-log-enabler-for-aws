74b65769ac6e35766b6c8034320c7b67bc3034ff
za <za@python.or.id>
2024-11-25 17:32:26 +0700

    Issue 66: sort regions alphabetically on new lines
    Issue #66
    https://github.com/awslabs/assisted-log-enabler-for-aws/issues/66
    
    Reason: it will be easier to see the diff if we want to add new region
    issue #64

--- subfunctions/ALE_dryrun_multi.py
+++ subfunctions/ALE_dryrun_multi.py
@@ -29,7 +29,29 @@ organizations = boto3.client('organizations')
 region = os.environ['AWS_REGION']
 
 
-region_list = ['af-south-1', 'ap-east-1', 'ap-south-1', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3', 'ap-southeast-1', 'ap-southeast-2', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2', 'eu-west-3', 'eu-north-1', 'eu-south-1', 'me-south-1', 'sa-east-1', 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2']
+region_list = [
+    'af-south-1',
+    'ap-east-1',
+    'ap-northeast-1',
+    'ap-northeast-2',
+    'ap-northeast-3',
+    'ap-south-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+    'ca-central-1',
+    'eu-central-1',
+    'eu-north-1',
+    'eu-south-1',
+    'eu-west-1',
+    'eu-west-2',
+    'eu-west-3',
+    'me-south-1',
+    'sa-east-1',
+    'us-east-1',
+    'us-east-2',
+    'us-west-1',
+    'us-west-2',
+    ]
 
 
 # 1. Obtain the AWS Accounts inside of AWS Organizations.
